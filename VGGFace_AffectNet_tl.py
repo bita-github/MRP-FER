95e00d0f5ea4927f4980974b05f6a5a03582d47c
Bita Houshmand <bitahoushmandr@gmail.com>
2020-08-24 16:24:04 -0400

    build custom VGG16 pretrained on VGGFace and fine-tune on AffectNet

--- VGGFace_AffectNet_tl.py
+++ VGGFace_AffectNet_tl.py
@@ -1,33 +1,33 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-import tensorflow 
-from keras.preprocessing.image import ImageDataGenerator 
+
 import numpy as np
 import pandas as pd
+import os 
+import random
+import copy
+import tensorflow 
+from keras.preprocessing.image import ImageDataGenerator 
 from tensorflow.keras import regularizers
-from keras.layers import Flatten, Dense, Input, GlobalAveragePooling2D,                          GlobalMaxPooling2D, Activation, Conv2D, MaxPooling2D, BatchNormalization,                         AveragePooling2D, Reshape, Permute, multiply
+from keras.layers import Flatten, Dense, Input, GlobalAveragePooling2D,\
+    GlobalMaxPooling2D, Activation, Conv2D, MaxPooling2D, BatchNormalization,\
+    AveragePooling2D, Reshape, Permute, multiply
 from keras_applications.imagenet_utils import _obtain_input_shape
 from keras.utils import layer_utils
 from keras.utils.data_utils import get_file
 from keras import backend as K
-from keras_vggface import utils
 from keras.engine.topology import get_source_inputs
 import warnings
 from keras.models import Model
 from keras import layers
-from keras_vggface.vggface import VGGFace
 import tensorflow
 from PIL import Image
 from tensorflow.keras.callbacks import CSVLogger, ModelCheckpoint, ReduceLROnPlateau, EarlyStopping
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import normalize, OneHotEncoder
 from sklearn.utils import class_weight
-import os 
-import random
-import copy
 from keras import Model
-from keras.regularizers import l1, l2, l1_l2
 import tensorflow
 import skimage.transform
 import cv2
@@ -36,9 +36,9 @@ from tensorflow.keras import layers, losses, optimizers
 from tensorflow.keras.layers import ZeroPadding2D, Convolution2D, MaxPooling2D, Dropout, Flatten, Activation
 from tensorflow.keras.models import Sequential, model_from_json, load_model 
 from keras.optimizers.schedules import InverseTimeDecay
-import keras
 
 plt.style.use('ggplot')
+
 
 #batch generator reference: https://keras.io/preprocessing/image/
 
@@ -63,49 +63,6 @@ def my_generator(gen_args, b_size=64, im_size = (224,224)):
                                     batch_size=b_size, target_size=im_size)
 
         return train_it, val_it
-
-
-# Whether to retrain the model or load a previously saved model.
-retrain = True
-cp_dir ='/home/ubuntu/Notebooks/Models/AffectNet_resnet_v1.h5'
-log_dir = '/home/ubuntu/Notebooks/Models/AffectNet_resnet_v1_log_.csv'
-
-if retrain:    
-  nb_class = 8
-  vgg_model = VGGFace(model='resnet50', include_top=True, input_shape=(224, 224, 3))
-  last_layer = vgg_model.get_layer('avg_pool').output
-  x = keras.layers.Flatten(name='flatten')(last_layer)
-  out = Dense(nb_class, activation='softmax', name='classifier')(x)
-  FER_VGGFaceResnet50 = Model(vgg_model.input, out)
-    
-  # compile the model
-  rlrop = ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=10, verbose=1)
-  opt = tensorflow.keras.optimizers.SGD(learning_rate=1e-3, momentum=0.9)
-  stop = EarlyStopping(monitor='val_accuracy', mode='max', patience=10, verbose=1)
-  FER_VGGFaceResnet50.compile(optimizer= opt, loss='categorical_crossentropy', metrics=['accuracy'])
-    
-  FER_VGGFaceResnet50.summary()
-else:
-  best_FER_VGGFaceResnet50 = load_model(cp_dir)
-  best_FER_VGGFaceResnet50.summary()
-
-# Fitting the model on the custom generator.
-if retrain:
-    # Defining paramters for image augmentation:
-    generator_args = {'rescale' : 1./255,
-                      'horizontal_flip':True}
-    train_iter, val_iter = my_generator(generator_args, b_size=64)
-    # Save model logs and the best model to a file.
-    model_log = CSVLogger(log_dir, separator=',') 
-    model_cp = keras.callbacks.ModelCheckpoint(cp_dir, save_best_only=True,
-                               monitor='val_loss', mode='min')
-
-    # Fit the model.
-    history = FER_VGGFaceResnet50.fit(x=train_iter, 
-                  validation_data=(val_iter),
-                  epochs=100,
-                  callbacks=[model_cp, model_log, rlrop, stop])
-    best_FER_VGGFaceResnet50 = load_model(cp_dir)  # Retrieve the best model.
 
 #functions for visualization of the results.
 
@@ -218,6 +175,134 @@ def plot_wrong_predictions_heatmap(targets, preds):
     plt.tight_layout()
     plt.show()
 
+
+# In[5]:
+
+
+# Whether to retrain the model or load a previously saved model.
+retrain = True
+cp_dir ='/home/ubuntu/Notebooks/Models/AffectNet_VGG16.h5'
+log_dir = '/home/ubuntu/Notebooks/Models/AffectNet_VGG16_log_.csv'
+
+
+# In[9]:
+
+
+if retrain:    
+    VGGFace16_classes = 2622
+    VGGFace16 = Sequential()
+
+    #Block 1
+    VGGFace16.add(layers.Conv2D(input_shape=(224,224,3),filters=64,kernel_size=(3,3),
+                         padding='same', activation='relu', name='conv1_1'))
+    VGGFace16.add(layers.Conv2D(filters=64,kernel_size=(3,3),
+                         padding='same', activation='relu', name='conv1_2'))
+    VGGFace16.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2), name='pool1'))
+
+    # Block 2
+    VGGFace16.add(layers.Conv2D(filters=128, kernel_size=(3, 3), activation='relu',
+                         padding='same', name='conv2_1'))
+    VGGFace16.add(layers.Conv2D(filters=128, kernel_size=(3, 3), activation='relu',
+                         padding='same', name='conv2_2'))
+    VGGFace16.add(layers.Dropout(0.1))
+    VGGFace16.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2), name='pool2'))
+
+    # Block 3
+    VGGFace16.add(layers.Conv2D(filters=256, kernel_size=(3, 3), activation='relu',
+                         padding='same', name='conv3_1'))
+    VGGFace16.add(layers.Dropout(0.15))
+    VGGFace16.add(layers.Conv2D(filters=256, kernel_size=(3, 3), activation='relu',
+                         padding='same', name='conv3_2'))
+    VGGFace16.add(layers.Dropout(0.20))
+    VGGFace16.add(layers.Conv2D(filters=256, kernel_size=(3, 3), activation='relu',
+                         padding='same', name='conv3_3'))
+    VGGFace16.add(layers.Dropout(0.25))
+    VGGFace16.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2), name='pool3'))
+
+    # Block 4
+    VGGFace16.add(layers.Conv2D(filters=512, kernel_size=(3, 3), activation='relu',
+                         padding='same', name='conv4_1'))
+    VGGFace16.add(layers.Dropout(0.30))
+    VGGFace16.add(layers.Conv2D(filters=512, kernel_size=(3, 3), activation='relu',
+                         padding='same', name='conv4_2'))
+    VGGFace16.add(layers.Dropout(0.35))
+    VGGFace16.add(layers.Conv2D(filters=512, kernel_size=(3, 3), activation='relu',
+                         padding='same', name='conv4_3'))
+    VGGFace16.add(layers.Dropout(0.40))
+    VGGFace16.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2), name='pool4'))
+
+    # Block 5
+    VGGFace16.add(layers.Conv2D(filters=512, kernel_size=(3, 3), activation='relu',
+                         padding='same', name='conv5_1'))
+    VGGFace16.add(layers.Dropout(0.45))
+    VGGFace16.add(layers.Conv2D(filters=512, kernel_size=(3, 3), activation='relu',
+                         padding='same', name='conv5_2'))
+    VGGFace16.add(layers.Dropout(0.50))
+    VGGFace16.add(layers.Conv2D(filters=512, kernel_size=(3, 3), activation='relu',
+                         padding='same', name='conv5_3'))
+    VGGFace16.add(layers.Dropout(0.55))
+    VGGFace16.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2), name='pool5'))
+    
+
+    # Classification block
+    VGGFace16.add(Flatten(name='flatten'))
+    VGGFace16.add(layers.Dense(units=512, name='fc6'))
+    VGGFace16.add(Activation(activation='relu', name='fc6/relu'))
+    VGGFace16.add(layers.Dense(units=512, name='fc7'))
+    VGGFace16.add(Activation(activation='relu', name='fc7/relu'))
+    VGGFace16.add(layers.Dense(8, name='fc8'))
+    VGGFace16.add(Activation(activation='softmax', name='fc8/softmax'))
+    
+    # load the pretrained weights
+    VGGFace16.load_weights('/home/ubuntu/Notebooks/Models/AffectNet_VGG16_v3-1.h5', by_name=True)
+    
+    # construct new classification layers
+    nb_class = 8
+    hidden_dim = 512
+    last_layer = VGGFace16.get_layer('pool5').output
+    x = Flatten(name='flatten')(last_layer)
+    x = layers.Dense(hidden_dim, activation='relu', name='fc6', kernel_regularizer=l2(5*1e-4))(x)
+    x = layers.Dense(hidden_dim, activation='relu', name='fc7', kernel_regularizer=l2(5*1e-4))(x)
+    out = layers.Dense(nb_class, activation='softmax', name='fc8')(x)
+    FER_VGGFace16 = tensorflow.keras.Model(VGGFace16.input, out )
+    
+    # compile the model
+
+#     initial_learning_rate = 1e-3
+#     decay_steps = 10
+#     decay_rate = 0.5
+#     rlrop = InverseTimeDecay(initial_learning_rate, decay_steps, decay_rate)
+
+    opt = tensorflow.keras.optimizers.SGD(learning_rate=1e-3, momentum=0.9)
+    FER_VGGFace16.compile(optimizer= opt, loss='categorical_crossentropy', metrics=['accuracy'])
+    
+    FER_VGGFace16.summary()
+else:
+    best_FER_VGGFace16 = load_model(cp_dir)
+    best_FER_VGGFace16.summary()
+
+# Fitting the model on the custom generator.
+if retrain:
+    # Defining paramters for image augmentation:
+    generator_args = {'rescale' : 1./255,
+                      'horizontal_flip':True}
+    train_iter, val_iter = my_generator(generator_args, b_size=64)
+    # Save model logs and the best model to a file.
+    model_log = CSVLogger(log_dir, separator=',') 
+    model_cp = ModelCheckpoint(cp_dir, save_best_only=True,
+                               monitor='val_loss', mode='min')
+    
+    # define early stopping and learning rate decay
+    rlrop = ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=5, verbose=1, cooldown=3)
+    stop = EarlyStopping(monitor='loss', patience=5)
+    
+    # Fit the model. class_weight=weights,
+    history = FER_VGGFace16.fit(x=train_iter, 
+                  validation_data=(val_iter),
+                  epochs=50,
+                  callbacks=[model_cp, model_log, rlrop, stop])
+    FER = load_model(cp_dir)  # Retrieve the best model.
+
 out = pd.read_csv(log_dir, delimiter=',')
 plot_accuracy(out)
 plot_loss(out)
@@ -237,7 +322,7 @@ filenames = test_iter.filenames
 nb_samples = len(filenames)
 
 #prediction
-predict = best_FER_VGGFaceResnet50.predict(test_iter, steps = nb_samples, verbose=1)
+predict = FER.predict(test_iter, steps = nb_samples, verbose=1)
 
 # create a dataframe for images and their corresponding predicted target 
 predicted_class_indices=np.argmax(predict,axis=1)
